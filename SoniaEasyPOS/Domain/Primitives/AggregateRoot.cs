2919c13b553e9255836c03ff23fca3d1b7133cfd
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-06-15 10:55:33 -0600

    Guardar cambios

--- SoniaEasyPOS/Domain/Primitives/AggregateRoot.cs
+++ SoniaEasyPOS/Domain/Primitives/AggregateRoot.cs
@@ -2,5 +2,13 @@ namespace Domain.Primitives;
 
 public abstract class AggregateRoot
 {
+private readonly List<DomaintEvent> _domainEvents = new();
+
+public ICollection<DomainEvent> GetDomainEvents() => _domainEvents;
+
+protected void Raise (DomainEvent domainEvent)
+{
+    _domainEvents.Add(domainEvent);
+}
 
 }
